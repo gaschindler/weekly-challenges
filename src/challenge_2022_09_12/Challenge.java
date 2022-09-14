af49a63b4ece44823fb50cfcf94d508ce7791c9b
gaschindler <gaschindler2018@gmail.com>
2022-09-14 14:10:47 -0400

    added reorderDigits solution

--- src/challenge_2022_09_12/Challenge.java
+++ src/challenge_2022_09_12/Challenge.java
@@ -1,9 +1,17 @@
 package challenge_2022_09_12;
 
+import java.util.Arrays;
+
 public class Challenge {
 
 	public static void main(String[] args) {
 		System.out.println("----- reorderDigits() Tests -----");
+		System.out.println(Arrays.toString(reorderDigits(new int[] {515, 341, 98, 44, 211}, "asc")));
+		System.out.println(Arrays.toString(reorderDigits(new int[] {515, 341, 98, 44, 211}, "desc")));
+		System.out.println(Arrays.toString(reorderDigits(new int[] {63251, 78221}, "asc")));
+		System.out.println(Arrays.toString(reorderDigits(new int[] {63251, 78221}, "desc")));
+		System.out.println(Arrays.toString(reorderDigits(new int[] {1, 2, 3, 4}, "asc")));
+		System.out.println(Arrays.toString(reorderDigits(new int[] {1, 2, 3, 4}, "desc")));
 		
 		System.out.print("\n");
 		
@@ -14,7 +22,46 @@ public class Challenge {
 	}
 	
 	public static int[] reorderDigits(int[] numbers, String method) {
-		return null;
+		for (int i = 0; i < numbers.length; i++) {
+			char[] currentNumber = String.valueOf(numbers[i]).toCharArray();
+			
+			// selection sort on currentNumber
+			if (method.equals("asc")) {
+				for (int j = 0; j < currentNumber.length; j++) {
+					int minIndex = j;
+					
+					for (int k = j + 1; k < currentNumber.length; k++) {
+						if (currentNumber[k] < currentNumber[minIndex]) {
+							minIndex = k;
+						}
+					}
+					
+					char temp = currentNumber[j];
+					currentNumber[j] = currentNumber[minIndex];
+					currentNumber[minIndex] = temp;
+				}
+			} else if (method.equals("desc")) {
+				for (int j = 0; j < currentNumber.length; j++) {
+					int maxIndex = j;
+					
+					for (int k = j + 1; k < currentNumber.length; k++) {
+						if (currentNumber[k] > currentNumber[maxIndex]) {
+							maxIndex = k;
+						}
+					}
+					
+					char temp = currentNumber[j];
+					currentNumber[j] = currentNumber[maxIndex];
+					currentNumber[maxIndex] = temp;
+				}
+			} else {
+				return null;
+			}
+			
+			numbers[i] = Integer.parseInt(new String(currentNumber));
+		}
+		
+		return numbers;
 	}
 	
 	public static boolean canPartition(int[] numbers) {
